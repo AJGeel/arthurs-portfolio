5dd35476e3421b900999bfe0ded9c8645fdc5265
Arthur Geel <geelaj01@gmail.com>
2024-10-11 12:55:43 +0200

    Add dynamic import for DVD Screensaver, highscore

--- src/app/(bare)/screensaver/page.tsx
+++ src/app/(bare)/screensaver/page.tsx
@@ -1,71 +1,11 @@
 "use client";
 
-import { StarIcon } from "@heroicons/react/24/outline";
-import { useEffect, useRef, useState } from "react";
-import { useDvdScreensaver } from "react-dvd-screensaver";
+import dynamic from "next/dynamic";
 
-import Me from "@/components/Me";
-import { Paragraph } from "@/components/Typography";
+const DvdScreensaver = dynamic(() => import("@/components/DvdScreensaver"), {
+  ssr: false,
+});
 
-const SCORE_BASE_RATE = 50;
-const SCORE_MULTIPLIER = 0.25;
-
-const ScreensaverPage = () => {
-  const { containerRef, elementRef } = useDvdScreensaver();
-  const [isWinning, setIsWinning] = useState(false);
-  const [score, setScore] = useState(0);
-  const scoreIntervalRef = useRef<number | null>(null);
-
-  const startScoring = () => {
-    if (!isWinning) {
-      setScore(1);
-      setIsWinning(true);
-    }
-
-    scoreIntervalRef.current = window.setInterval(() => {
-      setScore(
-        (prevScore) =>
-          prevScore + SCORE_BASE_RATE + prevScore * SCORE_MULTIPLIER
-      );
-    }, 100);
-  };
-
-  const stopScoring = () => {
-    setIsWinning(false);
-    if (scoreIntervalRef.current) {
-      clearInterval(scoreIntervalRef.current);
-    }
-  };
-
-  useEffect(
-    () => () => {
-      if (scoreIntervalRef.current) {
-        clearInterval(scoreIntervalRef.current);
-      }
-    },
-    []
-  );
-
-  return (
-    <div ref={containerRef} className="h-screen w-screen ">
-      <div className="absolute inset-0 flex size-full flex-col items-center justify-end">
-        <div className="flex w-full items-center justify-between gap-2 p-4">
-          <div className="flex items-center gap-2 rounded-md bg-white/60 px-3 py-2.5 shadow-md outline outline-1 outline-black/10">
-            <StarIcon className="size-4 shrink-0" />
-            <Paragraph>{Math.floor(score).toLocaleString()} points</Paragraph>
-          </div>
-        </div>
-      </div>
-      <span
-        className="z-20 inline-block size-20 cursor-grab will-change-transform"
-        ref={elementRef}
-        onMouseEnter={startScoring}
-        onMouseLeave={stopScoring}
-      >
-        <Me className="shadow-lg outline outline-1 outline-black/10" />
-      </span>
-    </div>
-  );
-};
+const ScreensaverPage = () => <DvdScreensaver />;
 
 export default ScreensaverPage;
---
+++ src/components/DvdScreensaver/DvdScreensaver.tsx
@@ -0,0 +1,123 @@
+import { StarIcon, TrophyIcon } from "@heroicons/react/24/outline";
+import React, { useEffect, useRef, useState } from "react";
+import { useDvdScreensaver } from "react-dvd-screensaver";
+
+import { cn } from "@/utils";
+
+import Me from "../Me";
+import { Paragraph } from "../Typography";
+
+const SCORE_BASE_RATE = 50;
+const SCORE_MULTIPLIER = 0.25;
+
+const ScorePanel = ({
+  text,
+  icon,
+  label,
+}: {
+  text: string;
+  icon: "star" | "trophy";
+  label: string;
+}) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      className="group relative flex items-center gap-2 rounded-md bg-white/60 px-3 py-2.5 shadow-md outline outline-1 outline-black/10"
+    >
+      {icon === "star" ? (
+        <StarIcon className="size-4 shrink-0" />
+      ) : (
+        <TrophyIcon className="size-4 shrink-0" />
+      )}
+      <Paragraph>{text}</Paragraph>
+      {isHovering && (
+        <div
+          className={cn(
+            "absolute -top-8 left-0 flex w-full flex-col items-center"
+          )}
+        >
+          <div className="text-nowrap rounded-md bg-white/60 px-2 py-1 text-center text-xs shadow-lg outline outline-1 outline-black/10">
+            {label}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const DvdScreensaver = () => {
+  const { containerRef, elementRef } = useDvdScreensaver();
+  const scoreIntervalRef = useRef<number | null>(null);
+
+  const [isWinning, setIsWinning] = useState(false);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  const startScoring = () => {
+    if (!isWinning) {
+      setScore(1);
+      setIsWinning(true);
+    }
+
+    scoreIntervalRef.current = window.setInterval(() => {
+      setScore((prevScore) =>
+        Math.floor(prevScore + SCORE_BASE_RATE + prevScore * SCORE_MULTIPLIER)
+      );
+    }, 100);
+  };
+
+  const stopScoring = () => {
+    setIsWinning(false);
+
+    if (scoreIntervalRef.current) {
+      clearInterval(scoreIntervalRef.current);
+
+      if (score > highScore) {
+        setHighScore(score);
+      }
+    }
+  };
+
+  useEffect(
+    () => () => {
+      if (scoreIntervalRef.current) {
+        clearInterval(scoreIntervalRef.current);
+      }
+    },
+    []
+  );
+
+  return (
+    <div ref={containerRef} className="h-screen w-screen ">
+      <div className="absolute inset-0 flex size-full flex-col items-center justify-end">
+        <div className="flex w-full items-center gap-4 p-4">
+          <ScorePanel
+            text={`${score.toLocaleString()} points`}
+            icon="star"
+            label="Current score"
+          />
+          {!!highScore && (
+            <ScorePanel
+              text={`${highScore.toLocaleString()} points`}
+              icon="trophy"
+              label="Highscore"
+            />
+          )}
+        </div>
+      </div>
+      <span
+        className="z-20 inline-block size-20 cursor-grab will-change-transform"
+        ref={elementRef}
+        onMouseEnter={startScoring}
+        onMouseLeave={stopScoring}
+      >
+        <Me className="shadow-lg outline outline-1 outline-black/10" />
+      </span>
+    </div>
+  );
+};
+
+export default DvdScreensaver;
